35eb5c990fd39441acf8d017b90330c6f2591fd1
Jim Chiang <jchiang@slac.stanford.edu>
2016-04-17 15:17:27 -0700

    add basic db functionality

--- python/desc/pserv/Pserv.py
+++ python/desc/pserv/Pserv.py
@@ -1,11 +1,80 @@
-class Pserv(object):
-    '''
-    Example class for Pserv package.
-    '''
-    def __init__(self, message):
-        self.message = message
+"""
+Pserv: Practice LSST database server code.
+"""
+import copy
+import json
+import MySQLdb
 
-    def run(self, raise_error=False):
-        if raise_error:
-            raise RuntimeError()
-        return self.message
+
+def _nullFunc(*args):
+    """
+    Default do-nothing function for processing data from a MySQLdb
+    cursor object.
+    """
+    return None
+
+class LsstDbConnection(object):
+    """
+    Class to manage MySQL connections using Borg attribute management.
+    """
+    __connection_pool = dict()
+    __connection_refs = dict()
+    def __init__(self, **kwds):
+        """
+        Constructor to make the connection object.
+        """
+        self._get_mysql_connection(kwds)
+
+    def __del__(self):
+        """
+        Decrement reference counts to the connection object and close
+        it if ref counts is zero.
+        """
+        self.__connection_refs[self._conn_key] -= 1
+        if self.__connection_refs[self._conn_key] == 0:
+            self._mysql_connection.close()
+            del self.__connection_pool[self._conn_key]
+            del self.__connection_refs[self._conn_key]
+
+    def _get_mysql_connection(self, kwds_par):
+        """
+        Update the connection pool and reference counts, and set the
+        self._mysql_connection reference.
+        """
+        kwds = copy.deepcopy(kwds_par)
+        try:
+            del kwds['table_name']
+        except KeyError:
+            pass
+        # Serialize the kwds dict to obtain a hashable key for the
+        # self.__connection_pool and self.__connection_refs dicts.
+        self._conn_key = json.dumps(kwds, sort_keys=True)
+
+        if not self.__connection_pool.has_key(self._conn_key):
+            # Create a new mysql connection object.
+            self.__connection_pool[self._conn_key] = MySQLdb.connect(**kwds)
+
+        # Update the reference counts for the connection objects.
+        try:
+            self.__connection_refs[self._conn_key] += 1
+        except KeyError:
+            self.__connection_refs[self._conn_key] = 1
+
+        self._mysql_connection = self.__connection_pool[self._conn_key]
+
+    def apply(self, query, cursorFunc=_nullFunc):
+        """
+        Apply the query, optionally using the cursorFunc to process
+        the query results.
+        """
+        cursor = self._mysql_connection.cursor()
+        try:
+            cursor.execute(query)
+            results = cursorFunc(cursor)
+        except MySQLdb.DatabaseError, message:
+            cursor.close()
+            raise MySQLdb.DatabaseError(message)
+        cursor.close()
+        if cursorFunc is _nullFunc:
+            self._mysql_connection.commit()
+        return results
--- tests/test_Pserv.py
+++ tests/test_Pserv.py
@@ -1,24 +1,107 @@
 """
-Example unit tests for Pserv package
+Unit tests for pserv package.
 """
 import unittest
+from warnings import filterwarnings
+import MySQLdb as Database
 import desc.pserv
 
+# Suppress warnings from database module.
+filterwarnings('ignore', category=Database.Warning)
+
+def get_db_info():
+    """
+    Try to connect to Travis CI MySQL services or the user's via
+    ~/.my.cnf and return the connection info.  Otherwise, return
+    an empty dict, which should skip the tests.
+    """
+    db_info = {}
+    try:
+        try:
+            # Travis CI usage:
+            my_db_info = dict(db='myapp_test', user='travis', host='127.0.0.1')
+            test = Database.connect(**my_db_info)
+        except Exception, eobj:
+            #print eobj
+            # User's configuration:
+            my_db_info = dict(db='test', read_default_file='~/.my.cnf')
+            test = Database.connect(**my_db_info)
+        test.close()
+        db_info = my_db_info
+    except Exception, eobj:
+        print "No database connection:"
+        print eobj
+    return db_info
+
+_db_info = get_db_info()
+
+@unittest.skipUnless(_db_info, "MySQL database not available")
 class PservTestCase(unittest.TestCase):
     def setUp(self):
-        self.message = 'Hello, world'
-        
+        """
+        Create a connection and test table.
+        """
+        global _db_info
+        self.connection = desc.pserv.LsstDbConnection(**_db_info)
+        self.test_table = 'my_test'
+        self.connection.apply('drop table if exists %s;' % self.test_table)
+        self.create_test_table()
+
     def tearDown(self):
+        """
+        Drop the test table and close the connection.
+        """
+        self.connection.apply('drop table if exists %s;' % self.test_table)
+        del self.connection
+
+    def create_test_table(self):
+        """
+        Create and fill test table with key/value pairs.
+        """
+        table_name = self.test_table
+        self.data = dict(a=1, b=3, c=4, d=0)
+        query = 'create table %s (keywd char(1), value int);' % self.test_table
+        self.connection.apply(query)
+        values = ','.join(["('%s', %i)" % kv for kv in self.data.items()]) + ';'
+        query = "insert into %(table_name)s values %(values)s" % locals()
+        self.connection.apply(query)
+
+    def test_apply_cursorFunc(self):
+        """
+        Test the apply method using a cursor function to retrieve and
+        package the query results.
+        """
+        def my_func(curs):
+            "Retrieve key/value pairs as a dict."
+            return dict([row for row in curs])
+        query = "select keywd, value from %s" % self.test_table
+        table_data = self.connection.apply(
+            query, cursorFunc=lambda curs: dict([row for row in curs]) )
+        self.assertEqual(self.data, table_data)
+
+    def test_connection_management(self):
+        """
+        White-box tests of connection pool management.
+        """
+        global _db_info
+        num_cobjs = 3
+        connection_objects = [desc.pserv.LsstDbConnection(**_db_info)
+                              for i in range(num_cobjs)]
+        for cobj in connection_objects:
+            key = cobj._conn_key
+            # All of the connection objects should be the same since
+            # they were built using the same _db_info dict.
+            self.assertEqual(self.connection._mysql_connection,
+                             cobj._mysql_connection)
+            # self.connection is included in the reference count so
+            # subtract it from the current count for the comparison.
+            self.assertEqual(cobj._LsstDbConnection__connection_refs[key] - 1,
+                             len(connection_objects))
+            cobj_last = connection_objects.pop()
+            del cobj_last
+
+    def test_csv_load(self):
         pass
-
-    def test_run(self):
-        foo = desc.pserv.Pserv(self.message)
-        self.assertEquals(foo.run(), self.message)
-
-    def test_failure(self):
-        self.assertRaises(TypeError, desc.pserv.Pserv)
-        foo = desc.pserv.Pserv(self.message)
-        self.assertRaises(RuntimeError, foo.run, True)
 
 if __name__ == '__main__':
     unittest.main()
